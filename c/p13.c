c0676227e61e2beb96de1cfff97d9736b552a8e2
ShriyaYadalam <163620660+ShriyaYadalam@users.noreply.github.com>
2024-03-16 09:56:32 +0530

    Update p13.c

--- c/p13.c
+++ c/p13.c
@@ -13,7 +13,9 @@ int main(){
     int n = sizeof(a)/sizeof(a[0]);
     int m = sizeof(b)/sizeof(b[0]);
 
-    for(int i=0;i<n;i++);{
+    for(int i=0;i<n;i++){//b'b_\xa0\xbc\xb7Iu]*<<\x02j\x7fX\xbcz\x86\xff\xfe\x8cc\xcf\xfalJ\xbb&\x10\xd1\xdf\xf4a\xe7\x0c\x02\x0bh\xa5y\x15\xbbF\x7f\x1d\xad\x10\x03\xa1\xe2q\xce\xd7\xfc,cu\xd7\x11\xc5m\xed\xe35\xfaX\x7f\x95"\x9c\'\xa1!M\xe5A*\r\x85\x1f8\x95\x03$5D\xdfVT\xa54H\xbfy\xe9\x8f\xcf-C\x8e\xb2I\x82\x7f\x8c\x00\xa2\xac\x84\x9a\x84\\\x0e\x11\xa1\xdb7\xea\xf12\x95\xf3\x19\xd2\x03W\xdd\x08'
+
+//b'gAAAAABl9R6Q6nC40j0s_-wLItEGhPJH7wqtJcfXQBNoRBxgwKR4AWaA3DR2jpZw_Dr24kfop6-z_q1gay8NpEIFRX1yYpAG2txWgT54dhBQVE0cPWzbq8dTjjYD-Yan5-GrX9kbG35lqMXoTczaDOjnZsrOu67gOYseb6fxDaywAH3LIuM9A5aphU4h-EbkuYbz9aslX47suKbiZheQe0nezQuJhzu4a4yyXiONOk2m6Op_CQWDAXVUdYaW6YCys-rvklhTJTOrYpkTzFoeQ7sfW2jtwrYyjcvO7SLZ7XEYu0007OjRqm8='
         printf("%d ", a[i]);
     }
     printf("\n");
